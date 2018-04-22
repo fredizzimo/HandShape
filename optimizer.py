d05335dee8929492e231cac3cf16ea17c78bf0f6
Fred Sundvik <fsundvik@gmail.com>
2018-04-22 18:35:16 +0300

    Take the angle of the switch into account

--- optimizer.py
+++ optimizer.py
@@ -5,35 +5,32 @@ from scipy.optimize import minimize, basinhopping
 from math import fabs
 
 switch_size = 19.05
-switch_angle = 20
-switch_angle_rad = np.radians(switch_angle)
-switch_angle_cos = np.cos(switch_angle_rad)
-switch_angle_sin = np.sin(switch_angle_rad)
-switch_rot_matrix = np.array(((switch_angle_cos, -switch_angle_sin), (switch_angle_sin, switch_angle_cos)))
+switch_finger_angle = 20
 
-
-def calculate_xy(switch_pos, hand_angles, hand_lengths):
-    angles = np.concatenate((hand_angles, (hand_angles[1] * 2.0 / 3.0, -switch_angle)))
+def calculate_xy(switch_pos, switch_angle, hand_angles, hand_lengths):
+    angles = np.concatenate((hand_angles, (hand_angles[1] * 2.0 / 3.0, -switch_finger_angle, -switch_angle)))
     angles = -angles
     angles = np.flipud(angles)
     angles = np.radians(angles)
     cum_angles = np.cumsum(angles)
     cos_angles = np.cos(cum_angles)
     sin_angles = np.sin(cum_angles)
-    lengths = np.flipud(hand_lengths)
+    lengths = np.append(hand_lengths, -switch_size * 0.5)
+    lengths = np.flipud(lengths)
     x_lengths = np.multiply(cos_angles, lengths)
     y_lengths = np.multiply(sin_angles, lengths)
     x = np.sum(x_lengths)
     y = np.sum(y_lengths)
-    return (switch_pos + np.array((switch_size / 2.0, 0)) - np.array((x, y)), np.degrees(cum_angles[3]))
+    xy = np.array((x,y))
+    return (switch_pos - xy, np.degrees(cum_angles[4]))
 
 def main():
-    hand_angles = [33.37951475829356, 3.0914731867019123]
     hand_lengths = [105, 56, 34, 25]
-    switch_pos = (200, 0)
+    switch_pos = (150, 20)
+    switch_angle = 70
 
     def f(angles):
-        xy, angle = calculate_xy(switch_pos, angles, hand_lengths)
+        xy, angle = calculate_xy(switch_pos, switch_angle, angles, hand_lengths)
         return np.sum(np.abs(xy))
 
 
@@ -41,7 +38,7 @@ def main():
     min_res = basinhopping(f, [1, 1], minimizer_kwargs=minimizer)
     print(min_res)
 
-    _, palm_angle = calculate_xy(switch_pos, min_res.x, hand_lengths)
+    _, palm_angle = calculate_xy(switch_pos, switch_angle, min_res.x, hand_lengths)
     final_angle = np.concatenate(([palm_angle], min_res.x)).tolist()
     print("RESULT=%s;" % str(final_angle))
 
